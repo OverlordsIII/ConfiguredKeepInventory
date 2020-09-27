dfccf24ce85d8727f511152a3bbab28b1afaa0af
OverlordsIII <OverlordsIII@users.noreply.github.com>
2020-09-27 14:02:26 -0700

    Added support to disable vanishing and binding curses

--- src/main/java/io/github/overlordsiii/config/InventoryConfig.java
+++ src/main/java/io/github/overlordsiii/config/InventoryConfig.java
@@ -19,4 +19,8 @@ public class InventoryConfig implements ConfigData {
     public List<String> itemsSavedList = new ArrayList<>();
     @Comment("Specifies which names of items will be saved and never lost. Has to be set through a custom name (such as from an anvil), and does not work on curse of vanishing items")
     public List<String> namesSavedList = new ArrayList<>();
+    @Comment("Makes vanishing not work. Will apply to other config features on this list, so if this feature is on, items with vanishing will be saved")
+    public boolean disableVanishingCurse = false;
+    @Comment("Makes Binding curses ineffective. This means that even if an item has binding is can still be moved around the inventory if this is turned on")
+    public boolean disableBindingCurse = false;
 }
--- src/main/java/io/github/overlordsiii/mixin/EnchantmentHelperMixin.java
+++ src/main/java/io/github/overlordsiii/mixin/EnchantmentHelperMixin.java
@@ -1,5 +1,6 @@
 package io.github.overlordsiii.mixin;
 
+import io.github.overlordsiii.ConfiguredKeepInventory;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,10 +14,14 @@ public abstract class EnchantmentHelperMixin {
 
     @Inject(method = "hasVanishingCurse", at = @At("HEAD"), cancellable = true)
     private static void vanishdisable(ItemStack stack, CallbackInfoReturnable<Boolean> cir){
-
+        if (ConfiguredKeepInventory.Config.enableConfig && ConfiguredKeepInventory.Config.disableVanishingCurse){
+            cir.setReturnValue(false);
+        }
     }
     @Inject(method = "hasBindingCurse", at = @At("HEAD"), cancellable = true)
     private static void bindingdisable(ItemStack stack, CallbackInfoReturnable<Boolean> cir){
-
+        if (ConfiguredKeepInventory.Config.enableConfig && ConfiguredKeepInventory.Config.disableBindingCurse){
+            cir.setReturnValue(false);
+        }
     }
 }
